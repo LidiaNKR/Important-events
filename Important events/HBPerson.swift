42f8e1975b060bbb0b17d743f624ca2c03fa1298
LidiaNKR <77931445+LidiaNKR@users.noreply.github.com>
2021-02-20 17:04:07 +0300

    setupStruct

---
+++ Important events/HBPerson.swift
@@ -0,0 +1,16 @@
+//
+//  HBPerson.swift
+//  Important events
+//
+//  Created by Лидия Ладанюк on 20.02.2021.
+//
+
+import Foundation
+
+struct HBPerson {
+    let name: String
+    let surname: String
+    let happyBirthdayDate: String
+    
+    
+}
